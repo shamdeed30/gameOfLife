271059be3116d79bf9482cf70f9573b5c30fb6b7
shamdeed30 <117672677+shamdeed30@users.noreply.github.com>
2023-09-26 20:17:53 +0200

    Add files via upload

---
+++ gol.java
@@ -0,0 +1,11 @@
+//package GOL;
+
+public class gol {
+
+   public static void main(String[] args) {
+  	   GameOfLife life = new GameOfLife();
+	   life.setLocationRelativeTo(life);;
+	   life.show();;
+   }
+   
+}
